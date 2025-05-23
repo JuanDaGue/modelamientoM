87dca36266bcb3575725bb2da038a05c3fbf36be
JuanDaGue <dj.jd2009@hotmail.com>
2025-05-23 14:52:22 -0500

    Add prefabs

---
+++ Assets/Scripts/Gun1.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class Gun1 : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public float gravity = -9.81f;
+    public float timeToLive = 5f;
+    public int resolution = 30; // Number of points to draw the trajectory
+
+    private LineRenderer lineRenderer;
+    private Camera playerCamera;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        playerCamera = Camera.main;
+        lineRenderer.positionCount = resolution;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) // Adjust input as needed
+        {
+            ShootProjectile();
+        }
+    }
+
+    void ShootProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+        Vector3 shootDirection = playerCamera.transform.forward;
+        Vector3 initialVelocity = shootDirection * speed;
+        initialVelocity.y += Mathf.Abs(gravity) * 0.5f; // Add vertical arc
+
+        rb.linearVelocity = initialVelocity;
+        Destroy(projectile, timeToLive);
+
+        DrawTrajectory(firePoint.position, initialVelocity);
+    }
+
+    void DrawTrajectory(Vector3 start, Vector3 initialVelocity)
+    {
+        Vector3[] points = new Vector3[resolution];
+
+        for (int i = 0; i < resolution; i++)
+        {
+            float time = (i / (float)resolution) * timeToLive;
+            points[i] = start + initialVelocity * time + 0.5f * new Vector3(0, gravity, 0) * time * time;
+        }
+
+        lineRenderer.SetPositions(points);
+    }
+}
--- Assets/Scripts/ParabolicGun.cs
+++ Assets/Scripts/ParabolicGun.cs
@@ -3,15 +3,55 @@ using UnityEngine;
 public class ParabolicGun : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
+    public float speed = 10;
+    public float angle = 0;
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public float gravity = -9.81f;
+    public float timeToLive = 5f;
+    public float maxDistance = 100f;
+    public float maxHeight = 50f;
+    public Vector3 InitialVelocity;
+    public Vector3 InitialPosition;
+
+
+
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InstantiateProjectile();
+
+    }
+    void InstantiateProjectile()
+    {
+        Vector3 direction = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle));
+        direction = direction.normalized;
+        RaycastHit hit;
+        if (Physics.Raycast(firePoint.position, direction, out hit, maxDistance, 1 << LayerMask.NameToLayer("Player")))
+        {
+            if (hit.distance < maxDistance)
+            {
+                Vector3 position = hit.point + direction * maxHeight;
+                GameObject projectile = Instantiate(projectilePrefab, position, Quaternion.identity) as GameObject;
+                projectile.transform.position = InitialPosition;
+                projectile.transform.rotation = firePoint.rotation;
+                projectile.GetComponent<Rigidbody>().linearVelocity = InitialVelocity;
+                projectile.GetComponent<Rigidbody>().AddForce(direction * speed);
+                projectile.GetComponent<Rigidbody>().AddForce(Vector3.up * gravity * Time.deltaTime);
+                Destroy(projectile, timeToLive);
+            }
+        }
+
+        angle += speed * Time.deltaTime;
+        if (angle > 360)
+        {
+            angle = 0;
+        }
     }
 }
